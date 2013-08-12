9380857d9773952306e090a00a0b5927d3faeb74
adammartin <ebondark.od@gmail.com>
2013-08-12 16:14:19 -0500

    Format clean up

--- src/test/java/com/pillar/pages/HomePage.java
+++ src/test/java/com/pillar/pages/HomePage.java
@@ -4,22 +4,22 @@ import com.pillar.driver.ModernWebDriver;
 
 public class HomePage extends BasePage {
 
-	public static final String HOME_PAGE_TITLE = "Welcome to BlueBox";
-	private static final String HOMEPAGE_HEADER_LINK = "div#blueBoxLogo a";
+  public static final String HOME_PAGE_TITLE = "Welcome to BlueBox";
+  private static final String HOMEPAGE_HEADER_LINK = "div#blueBoxLogo a";
 
-	public HomePage(final ModernWebDriver driver, final int maxWaitInSeconds) {
-		super(driver, maxWaitInSeconds);
-	}
+  public HomePage(final ModernWebDriver driver, final int maxWaitInSeconds) {
+    super(driver, maxWaitInSeconds);
+  }
 
-	public final void close() {
-		closeBrowser();
-	}
+  public final void close() {
+    closeBrowser();
+  }
 
-	public void returnToHomePage() {
-		clickLink(HOMEPAGE_HEADER_LINK);
-	}
+  public void returnToHomePage() {
+    clickLink(HOMEPAGE_HEADER_LINK);
+  }
 
-	public final String title() {
-		return driver().getTitle();
-	}
+  public final String title() {
+    return driver().getTitle();
+  }
 }
