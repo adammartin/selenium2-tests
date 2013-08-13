7d7d46a9a173b96648b8df5c16a66dd55590b60c
adammartin <ebondark.od@gmail.com>
2013-08-13 12:28:47 -0500

    More externalization of properties

--- src/test/java/com/pillar/pages/AuthenticationPage.java
+++ src/test/java/com/pillar/pages/AuthenticationPage.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebElement;
 import com.pillar.driver.ModernWebDriver;
 
 public class AuthenticationPage extends BasePage {
-  public static String PAGE_TITLE = "Login";
+  private static final String PAGE_TITLE = "selenium.auth.element.page.title";
   private static final String PASSWORD = "selenium.auth.element.name.password";
   private static final String USER_ID = "selenium.auth.element.name.user";
   private static final String INCORRECT_PAGE = "This is not the login page: ";
@@ -19,20 +19,20 @@ public class AuthenticationPage extends BasePage {
   public AuthenticationPage(final ModernWebDriver driver, final int maxWaitInSeconds, final Properties bundle) {
     super(driver, maxWaitInSeconds);
     this.bundle = bundle;
-    if (!PAGE_TITLE.equals(driver.getTitle())) {
+    final String pageTitle = bundle.getProperty(PAGE_TITLE);
+    if (pageTitle != null && !pageTitle.equals(driver.getTitle())) {
       throw new IllegalStateException(INCORRECT_PAGE + driver.getTitle());
     }
   }
 
   public final HomePage authenticate(final String userName, final String password) {
-    final WebElement passWordField = enterCredentials(userName, password);
-    passWordField.submit();
+    enterCredentials(userName, password).submit();
     return new HomePage(driver(), maxWait());
   }
 
   private final WebElement enterCredentials(final String userName, final String password) {
-    driver().findElement(name(bundle.getProperty(USER_ID))).sendKeys(userName);
     final WebElement passWordField = driver().findElement(name(bundle.getProperty(PASSWORD)));
+    driver().findElement(name(bundle.getProperty(USER_ID))).sendKeys(userName);
     passWordField.sendKeys(password);
     return passWordField;
   }
